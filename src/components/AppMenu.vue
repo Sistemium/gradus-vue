72f9137dfcd3531b790bc79b279426eaf45fe51d
Alexander Levin <alevin.ru@gmail.com>
2018-10-07 11:33:01 +0300

    init Catalogue

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -3,6 +3,9 @@
 el-menu(:default-active="$route.path" mode="horizontal" :router="true")
   el-menu-item(index="/")
     img(src="../assets/icons8-home.svg")
+  el-menu-item(index="/catalogue")
+    img(src="../assets/icons8-moleskine.svg")
+    span Каталог
   el-menu-item(index="/about")
     img(src="../assets/icons8-info.svg")
     span О проекте
---
+++ src/views/Catalogue.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+
+.catalogue
+  h1 Not implemented
+
+</template>
+<script>
+
+import ArticleGroup from '@/models/ArticleGroup';
+
+export default {
+  name: 'Catalogue',
+  data() {
+    return { articleGroups: [] };
+  },
+  async created() {
+    ArticleGroup.bindAll(this, {}, 'articleGroups');
+    await ArticleGroup.findAll({ limit: 10000 });
+  },
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+</style>
