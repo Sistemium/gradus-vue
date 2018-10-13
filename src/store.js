eb5b925f0abf21a38a7ee10e136401f7e4d7fe7d
Alexander Levin <alevin.ru@gmail.com>
2018-10-13 12:39:29 +0300

    sistemium-vue/store/auth

--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import auth from 'sistemium-vue/store/auth';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+
+  strict: process.env.NODE_ENV !== 'production',
+
   state: {
-
+    busy: false,
   },
-  mutations: {
 
+  modules: {
+    auth,
   },
-  actions: {
 
-  },
 });
