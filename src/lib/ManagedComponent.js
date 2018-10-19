be9cc4cd44ba90bb58de14d296e142ffa6fc5455
Alexander Levin <alevin.ru@gmail.com>
2018-10-19 19:01:36 +0300

    ManagedComponent stub

---
+++ src/lib/ManagedComponent.js
@@ -0,0 +1,31 @@
+import each from 'lodash/each';
+
+// import log from 'sistemium-telegram/services/log';
+
+// const { debug } = log('ManagedComponent');
+
+export default {
+
+  created() {
+
+    const { models } = this.$options;
+
+    each(models, model => {
+      // debug('created:', model.name);
+      model.bind(this);
+    });
+
+  },
+
+  beforeDestroy() {
+
+    const { models } = this.$options;
+
+    each(models, model => {
+      // debug('beforeDestroy:', model.name);
+      model.unbindAll(this);
+    });
+
+  },
+
+};
