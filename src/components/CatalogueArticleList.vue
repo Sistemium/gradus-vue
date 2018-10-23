26629a376a1026309de2543f1808b422e0f1a1a8
EdgarJan <krodanix@gmail.com>
2018-10-23 22:47:53 +0300

    virtual scroll

--- src/components/CatalogueArticleList.vue
+++ src/components/CatalogueArticleList.vue
@@ -2,19 +2,37 @@
 
 resize.list-group.catalogue-article-list(v-if="items.length" padding="35")
 
-  catalogue-article.list-group-item(
-  v-for="article in groupedItems()"
-  :key="article.id"
-  :article="article"
-  @avatar-click="$emit('avatar-click', article)"
+  DynamicScroller.scroller(
+  :items="groupedItems()"
+  :min-item-height="54"
   )
+
+    template(slot-scope='{ item, index, active }')
+      DynamicScrollerItem(
+      :item="item"
+      :active="active"
+      :size-dependencies="[item.name]"
+      :data-index="index"
+      )
+
+        catalogue-article.list-group-item(
+        :key="item.id"
+        :article="item"
+        @avatar-click="$emit('avatar-click', item)"
+        )
 
 </template>
 <script>
+import Vue from 'vue';
+import VueVirtualScroller from 'vue-virtual-scroller';
 
 import Article from '@/models/Article';
+
 import { groupedArticles } from '@/services/catalogue';
+
 import CatalogueArticle from './CatalogueArticle.vue';
+
+Vue.use(VueVirtualScroller);
 
 export default {
 
@@ -52,6 +70,10 @@ export default {
 .list-group-item {
   display: flex;
   padding: $margin-right;
+}
+
+.scroller{
+  height: 800px;
 }
 
 </style>
