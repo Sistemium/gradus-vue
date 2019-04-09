3bb0bf61b83e64d5d2898c7e925d94f329dc1075
Alexander Levin <alevin.ru@gmail.com>
2019-04-09 17:48:00 +0300

    Create ArticleSelect.vue

---
+++ src/components/ArticleSelect.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+
+v-select.article-select(
+:value="value"
+@input="onSelect"
+label="name"
+:filterable="false"
+:options="options"
+@search="onSearch"
+ref="select"
+)
+
+  template(slot="no-options") напишите часть названия, чтобы начать поиск
+
+  template(slot="option" slot-scope="option")
+    .name {{ option.name }}
+
+  template(slot="selected-option" slot-scope="option")
+    .name {{ option.name }}
+
+</template>
+<script>
+
+import debounce from 'lodash/debounce';
+import Article from '@/models/Article';
+
+export default {
+
+  name: 'ArticleSelect',
+
+  data() {
+    return {
+      options: [],
+      article: null,
+    };
+  },
+
+  props: {
+    value: Object,
+  },
+
+  methods: {
+
+    clear() {
+      // this.setOptions([]);
+      this.$refs.select.clearSelection();
+    },
+
+    onSelect(item) {
+      this.$emit('input', item);
+    },
+
+    onSearch(search, loading) {
+      loading(true);
+      this.search(loading, search, this);
+    },
+
+    search: debounce(async (loading, search, vm) => {
+      // vm.setOptions([{ name: 'test' }, { name: search }]);
+      const where = {
+        name: { likei: `%${search}%` },
+      };
+      const articles = await Article.findAll({ where });
+      vm.setOptions(articles);
+      loading(false);
+    }, 500),
+
+    setOptions(options) {
+      this.options = options;
+    },
+
+  },
+
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+.article-select {
+  /*overflow-y: auto;*/
+}
+
+
+</style>
