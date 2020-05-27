21a6bbdb7d8e0ee44fdc954dff39fac663d0c8d7
Alexander Levin <alevin.ru@gmail.com>
2020-05-28 00:05:50 +0300

    Create ButtonAdd.vue

---
+++ src/components/ButtonAdd.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+
+el-button.button-add(@click="$emit('click')" size="mini" icon="el-icon-circle-plus" circle)
+
+</template>
+<script>
+
+const NAME = 'ButtonAdd';
+
+export default {
+  name: NAME,
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+.button-add {
+  color: $primary-color;
+}
+
+</style>
