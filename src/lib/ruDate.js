e0858f779aef3c73a0876f09d87e86052720c7fa
Alexander Levin <alevin.ru@gmail.com>
2020-08-03 15:40:04 +0300

    ruDateTime filter

--- src/lib/ruDate.js
+++ src/lib/ruDate.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
-import { dateFormat } from 'sistemium-telegram/services/moments';
+import { dateFormat, dateTimeFormat } from 'sistemium-telegram/services/moments';
 
 Vue.filter('ruDate', ruDateFilter);
 
 function ruDateFilter(value) {
   return value && dateFormat(value);
+}
+
+Vue.filter('ruDateTime', ruDateTimeFilter);
+
+function ruDateTimeFilter(value) {
+  return value && dateTimeFormat(value);
 }
