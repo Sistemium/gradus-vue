a547c2d51ae79a5dd54630d359fc4291940ac3f0
Alexander Levin <alevin.ru@gmail.com>
2020-08-04 11:45:42 +0300

    Update ActionHistoryView.vue

--- src/components/actions/ActionHistoryView.vue
+++ src/components/actions/ActionHistoryView.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 
 .action-history-view(v-if="history.length")
+  .title
+    span История изменений
   .action-history(v-for="entry in history" :key="entry.id")
     .cts() {{ entry.cts | ruDateTime }}
     .comment() {{ entry.commentText }}
@@ -33,8 +35,19 @@ export default {
 .action-history {
   padding: $padding;
   display: flex;
+
   .cts {
     min-width: 140px;
+  }
+}
+
+.title {
+  span {
+    background: $orange;
+    color: white;
+    padding: 2px $padding;
+    border-bottom-right-radius: $border-radius;
+    margin-left: -1px;
   }
 }
 
