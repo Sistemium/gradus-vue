dc8b9b9c5b589849d7d905e6d00c9b1840696d6e
Alexander Levin <alevin.ru@gmail.com>
2020-09-07 15:42:36 +0300

    findByMany supports force

--- src/lib/modelExtentions.js
+++ src/lib/modelExtentions.js
@@ -5,13 +5,13 @@ import flatten from 'lodash/flatten';
 // eslint-disable-next-line import/prefer-default-export
 export async function findByMany(model, ids, options = {}) {
 
-  const { chunkSize = 100, field = 'id' } = options;
+  const { chunkSize = 100, field = 'id', force = false } = options;
 
   const chunks = chunk(uniq(ids), chunkSize);
 
   return Promise.all(chunks.map(chunkIds => {
     const where = { [field]: { in: chunkIds } };
-    return model.findAll({ where });
+    return model.findAll({ where }, { force });
   }))
     .then(flatten);
 
