95ece75722386f05a6c10815a2c6ec719a2c6d6e
Alexander Levin <alevin.ru@gmail.com>
2020-10-16 11:50:46 +0300

    Update CampaignDialog.vue

--- src/components/campaigns/CampaignDialog.vue
+++ src/components/campaigns/CampaignDialog.vue
@@ -1,98 +1,169 @@
 <template lang="pug">
 
-el-dialog.campaign-input(
-top="4vh"
-:title="title"
-:before-close="closeDialog"
-:visible.sync="visible"
-:append-to-body="true"
+el-dialog.campaign-dialog(
+  top="4vh"
+  :title="title"
+  :before-close="closeDialog"
+  :visible.sync="visible"
+  :append-to-body="true"
 )
 
   el-form(
-  :model="newCampaign"
-  ref="newCampaign"
-  :rules="rules"
-  size="mini"
+    :model="newCampaign"
+    ref="newCampaign"
+    :rules="rules"
+    size="mini"
   )
     el-form-item(label="Группа" prop="groupCode")
+      campaign-group-select(v-model="newCampaign.groupCode" ref="groupSelect")
 
-      campaign-group-select(v-model="newCampaign.groupCode")
+    el-form-item(label="Приоритет" prop="priorityId")
+      campaign-priority-select(v-model="newCampaign.priorityId")
 
     el-form-item(label="Название" prop="name")
-
       el-input(v-model="newCampaign.name")
 
-    //el-form-item(label="Ограничение по территории" prop="territory")
+    .period
 
-      el-input(v-model="newCampaign.territory")
+      el-form-item(label="Дата начала" prop="dateB")
+        el-date-picker(
+          v-model="newCampaign.dateB"
+          :picker-options = "{ disabledDate: disableMinDate }"
+          format="yyyy/MM/dd"
+          value-format="yyyy-MM-dd"
+        )
 
-    el-form-item(label="Дата начала" prop="dateB")
+      el-form-item(label="Дата окончания" prop="dateE")
+        el-date-picker(
+          v-model="newCampaign.dateE"
+          :picker-options= "{ disabledDate: disableMaxDate }"
+          format="yyyy/MM/dd"
+          value-format="yyyy-MM-dd"
+        )
 
-      el-date-picker(
-      v-model="newCampaign.dateB"
-      :picker-options = "{ disabledDate: disableMinDate }"
-      format="yyyy/MM/dd"
-      value-format="yyyy-MM-dd"
+    el-form-item(label="Комментарий" prop="commentText")
+      el-input(v-model="newCampaign.commentText" type="textarea" :autosize="{ minRows: 2 }")
+
+    .switches
+      el-switch(v-model="newCampaign.oneTime" inactive-text="Единовременная")
+      el-switch(v-model="newCampaign.repeatable" inactive-text="Многократная")
+      el-switch(v-model="newCampaign.needPhoto" inactive-text="Фото-отчет")
+
+    el-form-item.territory
+      el-input(
+        v-model="newCampaign.territory"
+        placeholder="ограничение по территории"
+        :clearable="true"
       )
+        template(slot="prepend")
+          i.el-icon-location
 
-    el-form-item(label="Дата окончания" prop="dateE")
+    .other(v-if="actions || pictures")
+      el-form-item
+        .actions(v-if="actions") Подчиненных акций: {{ actions.length }}
+        .actions(v-if="pictures") Изображений: {{ pictures.length }}
 
-      el-date-picker(
-      v-model="newCampaign.dateE"
-      :picker-options= "{ disabledDate: disableMaxDate }"
-      format="yyyy/MM/dd"
-      value-format="yyyy-MM-dd"
-      )
-
-    el-form-item(label="Версия" prop="version" v-if="isEdit()")
-
-      el-input-number(
-      v-model="newCampaign.version"
-      :min="minVersion()"
-      :max="maxVersion()"
-      )
-
-    el-form-item(label="Описание" prop="commentText")
-
-      el-input(v-model="newCampaign.commentText" type="textarea" :rows="4")
-
-    el-form-item
-      .buttons
-        confirm-button.remove(
-        v-if="isEdit()"
+    .buttons
+      confirm-button.remove(
+        size="small"
+        :class="!isEdit && 'hidden'"
         text="Удалить" confirm-text="Точно удалить?"
         @confirm="removeClick"
-        )
-        .editButtons
-          el-button(@click="closeDialog") Отмена
-          el-button(type="primary" @click="submitDialog('newCampaign')") Готово
+      )
+      .editButtons
+        el-button(@click="closeDialog" size="small") Отмена
+        el-button(type="primary" @click="submitDialog('newCampaign')" size="small") Готово
 
 </template>
 
 <script>
 
 import CampaignGroupSelect from '@/components/campaigns/CampaignGroupSelect.vue';
+import CampaignPrioritySelect from '@/components/campaigns/CampaignPrioritySelect.vue';
+import { campaignGroups } from '@/services/campaigns';
 
 export default {
 
   name: 'CampaignDialog',
-  components: { CampaignGroupSelect },
+  components: {
+    CampaignPrioritySelect,
+    CampaignGroupSelect,
+  },
   props: {
     campaign: Object,
   },
 
   data() {
     return {
-      newCampaign: {
-        name: '',
-        dateB: '',
-        dateE: '',
-        commentText: '',
-      },
-      rules: {
+      newCampaign: { ...this.campaign },
+      visible: true,
+    };
+  },
+
+  methods: {
+
+    closeDialog() {
+      this.newCampaign = {};
+      this.visible = false;
+      this.$emit('closed');
+    },
+
+    submitDialog(formName) {
+
+      this.$refs[formName].validate(valid => {
+
+        if (valid) {
+          this.$emit('submit', this.newCampaign);
+          this.closeDialog();
+        }
+
+        return valid;
+
+      });
+
+    },
+
+    disableMinDate(date) {
+      return !!(this.newCampaign.dateE && date > new Date(this.newCampaign.dateE));
+    },
+
+    disableMaxDate(date) {
+      return !!(this.newCampaign.dateB && date < new Date(this.newCampaign.dateB));
+    },
+
+    removeClick() {
+      this.$emit('remove', this.campaign);
+      this.closeDialog();
+    },
+
+  },
+
+  computed: {
+
+    isEdit() {
+      return !!this.campaign.id;
+    },
+
+    pictures() {
+      const { pictures } = this.newCampaign;
+      return pictures && pictures.length && pictures;
+    },
+
+    actions() {
+      const { actions } = this.newCampaign;
+      return actions && actions.length && actions;
+    },
+
+    title() {
+      return this.isEdit ? this.campaign.name : 'Новая Акция';
+    },
+
+    rules() {
+      return {
+        priorityId: [],
         groupCode: [
           {
-            required: true,
+            required: !!campaignGroups().length,
             message: 'Укажите группу акций',
             trigger: 'blur',
           },
@@ -118,82 +189,8 @@ export default {
             trigger: 'blur',
           },
         ],
-      },
-
-      visible: true,
-
-    };
-  },
-
-  methods: {
-
-    closeDialog() {
-
-      this.newCampaign = {};
-      this.visible = false;
-      this.$emit('closed');
-
+      };
     },
-
-    async submitDialog(formName) {
-
-      this.$refs[formName].validate(valid => {
-
-        if (valid) {
-          this.$emit('submit', this.newCampaign);
-          this.closeDialog();
-        }
-
-        return valid;
-
-      });
-
-    },
-
-    disableMinDate(date) {
-      return !!(this.newCampaign.dateE && date > new Date(this.newCampaign.dateE));
-    },
-
-    disableMaxDate(date) {
-      return !!(this.newCampaign.dateB && date < new Date(this.newCampaign.dateB));
-    },
-
-    removeClick() {
-
-      this.$emit('remove', this.campaign);
-      this.closeDialog();
-
-    },
-
-    isEdit() {
-      return this.campaign.id;
-    },
-
-    minVersion() {
-      return this.campaign.version || 1;
-    },
-
-    maxVersion() {
-      return (this.campaign.version || 1) + 1;
-    },
-
-  },
-
-  computed: {
-
-    title() {
-      return this.isEdit() ? this.campaign.name : 'Новая Акция';
-    },
-
-  },
-
-  created() {
-
-    this.newCampaign = { ...this.campaign };
-
-    if (!this.newCampaign.version) {
-      this.newCampaign.version = 1;
-    }
 
   },
 
@@ -203,28 +200,41 @@ export default {
 
 <style scoped lang="scss">
 
-.campaign-input {
+@import "../../styles/switches";
 
-  .el-input {
-    width: 100%;
-  }
+.campaign-dialog {
+  max-width: 1200px;
+}
 
-  .buttons {
+.el-input {
+  width: 100%;
+}
 
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row;
+.buttons {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+}
 
-  }
+.remove {
+  margin-right: 0;
+}
 
-  .remove {
-    margin-right: 0;
-  }
+.hidden {
+  visibility: hidden;
+}
 
-  .el-input-number {
-    width: 100%;
-  }
+.el-input-number {
+  width: 100%;
+}
 
+.period {
+  display: flex;
+  justify-content: space-between;
+}
+
+.switches {
+  @extend %switches;
 }
 
 </style>
