655f6da6355c11f1db21caf5d700977a7c366601
Alexander Levin <alevin.ru@gmail.com>
2020-10-16 09:45:30 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,71 @@
 module.exports = {
+  publicPath: '',
+  transpileDependencies: [
+    'sistemium-vue',
+    // 'sistemium-jsdata',
+    'vue-virtual-scroller',
+  ],
+  productionSourceMap: false,
   devServer: {
     proxy: {
-      '/api': {
-        target: 'https://api.sistemium.com/v4d/dr50',
+      // '/api/(.+)/Campaign($|/)': {
+      //   target: 'http://localhost:9090',
+      //   changeOrigin: true,
+      //   // pathRewrite: {
+      //   //   '/api/([^/]+)': '',
+      //   // },
+      // },
+      // '/api/(.+)/Action($|/)': {
+      //   target: 'http://localhost:9900/api',
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     '/api/([^/]+)': '',
+      //   },
+      // },
+      // '/api/r50': {
+      //   // target: 'https://api.sistemium.com/v4d/dr50',
+      //   // target: 'http://localhost:9090/api/dr50',
+      //   target: 'https://m.sistemium.com/api/r50',
+      //   changeOrigin: true,
+      //   // pathRewrite: {
+      //   //   '^/api/vr': '/api',
+      //   // },
+      // },
+      // '/api/dr50/Campaign($|/)': {
+      //   // target: 'https://api.sistemium.com/v4d/dr50',
+      //   target: 'http://localhost:9090',
+      //   // target: 'https://m.sistemium.com/api/dr50',
+      //   changeOrigin: true,
+      //   // pathRewrite: {
+      //   //   '^/api/dr50': '/api',
+      //   // },
+      // },
+      // '/api/dr50': {
+      //   // target: 'https://api.sistemium.com/v4d/dr50',
+      //   // target: 'http://localhost:9090/api/dr50',
+      //   target: 'https://m.sistemium.com/api/dr50',
+      //   changeOrigin: true,
+      //   // pathRewrite: {
+      //   //   '^/api/dr50': '/api',
+      //   // },
+      // },
+      '/api/(.+)/': {
+        // target: 'https://api.sistemium.com/v4d/dr50',
         // target: 'http://localhost:9090/api/dr50',
+        target: 'https://m.sistemium.com',
         changeOrigin: true,
         // pathRewrite: {
-        //   '^/api/vr': '/api',
+        //   '^/api/dr50': '/api',
         // },
       },
-      '/ims': {
-        target: 'https://api.sistemium.com/ims',
+      '/ims/': {
+        target: 'https://api.sistemium.com',
+        // target: 'http://localhost:8088/api/image',
         changeOrigin: true,
-        pathRewrite: {
-          '^/ims': '',
-        },
+        // pathRewrite: {
+        //   '^/ims': '',
+        //   // '^/ims/dr50': '',
+        // },
       },
     },
   },
