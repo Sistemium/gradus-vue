666a4d2c7d9e7875a77093888e9fc98a5f1f60e1
Alexander Levin <alevin.ru@gmail.com>
2021-06-21 19:30:05 +0300

    Create CommentsView.vue

---
+++ src/components/CommentsView.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+
+.comments-view(v-if="comments")
+  .comment(v-for="(comment, idx) in comments" :key="idx")
+    i.el-icon-info
+    styled-comment(:text="comment.text")
+
+</template>
+<script>
+
+import StyledComment from '@/components/StyledComment.vue';
+
+export default {
+  name: 'CommentsView',
+  components: { StyledComment },
+  props: {
+    comments: Array,
+  },
+};
+
+</script>
+<style scoped lang="scss">
+
+@import "../styles/variables";
+
+.comment {
+
+  display: flex;
+
+  i {
+    margin-right: $margin-right;
+    color: $orange;
+  }
+
+  & + .comment {
+    margin-top: $margin-top;
+  }
+
+}
+
+</style>
